541a3cde90e5320a595f416b743082b9a02b1cd5
NemalFdo <dinethnemal2002@gmail.com>
2024-06-27 19:42:19 +0530

    Add routes

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home.jsx";
+import Signin from './pages/Signin.jsx';
+import About from './pages/About.jsx';
+import Signup from './pages/Signup.jsx';
+import Profile from './pages/Profile.jsx';
+import Header from './components/Header.jsx';
 
 export default function App() {
   return (
-    <div className='text-red-700'>App</div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
